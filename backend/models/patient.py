3780e6c82669009cbce72aef63592853d9864c30
Ibrahim Hafez <71508496+ibrahimymhafez@users.noreply.github.com>
2025-12-26 19:11:44 +0200

    Update patient.py
    Removed disease attribute and related methods (assign_doctor_id()) from Patient class. Added methods to fetch all patients and search patients by name or ID.

--- backend/models/patient.py
+++ backend/models/patient.py
@@ -1,51 +1,43 @@
 from backend.models.person import Person
 
 class Patient(Person):
-    def __init__(self, name, age, gender, phone, email, disease):
+    def __init__(self, name, age, gender, phone, email):
         super().__init__(name, age, gender, phone, email)
-        self.disease = disease
-    
-    @staticmethod
-    def assign_doctor(cursor, conn, patient_id, doctor_id):
-        query = "SELECT * FROM patients WHERE patient_id = ?"
-        cursor.execute(query, (patient_id,))
-        patient = cursor.fetchone()
 
-        query = "SELECT * FROM doctors WHERE doctor_id = ?"
-        cursor.execute(query, (doctor_id,))
-        doctor = cursor.fetchone()
-
-        if patient and doctor:
-            query = "UPDATE patients SET assigned_doctor_id = ? WHERE patient_id = ?"
-            values = (doctor_id, patient_id)
-            cursor.execute(query, values)
-            conn.commit()
-            print(f"Doctor with ID: {doctor_id} is assigned to patient with ID: {patient_id} successfully")
-        else:
-            print("Doctor or Patient does not exits")
-            
     def save_to_db(self, cursor, conn):
-        values = (self.name, self.age, self.gender, self.phone, self.email, self.disease)
-        query = "INSERT INTO patients(name, age, gender, phone, email, disease) VALUES(?, ?, ?, ?, ?, ?)"
+        values = (self.name, self.age, self.gender, self.phone, self.email)
+        query = "INSERT INTO patients(name, age, gender, phone, email) VALUES(?, ?, ?, ?, ?)"
         cursor.execute(query, values)
         conn.commit()
-        self.patient_id = cursor.lastrowid
+        self.id = cursor.lastrowid
     
     @staticmethod
-    def delete_from_db(cursor, conn, patient_id):
-        query = "SELECT * FROM patients WHERE patient_id = ?"
-        cursor.execute(query, (patient_id,))
+    def fetch_all_patients(cursor):
+        query = "SELECT * FROM patients"
+        cursor.execute(query)
+        return cursor.fetchall()
+
+    @staticmethod
+    def search_patients(cursor, search_term):
+        query = "SELECT * FROM patients WHERE name LIKE ? OR id LIKE ?"
+        cursor.execute(query, (f"%{search_term}%", f"%{search_term}%"))
+        return cursor.fetchall()
+    
+    @staticmethod
+    def delete_from_db(cursor, conn, id):
+        query = "SELECT * FROM patients WHERE id = ?"
+        cursor.execute(query, (id,))
         found = cursor.fetchone()
         if found != None:
-            query = "DELETE FROM patients WHERE patient_id = ?"
-            cursor.execute(query, (patient_id,))
+            query = "DELETE FROM patients WHERE id = ?"
+            cursor.execute(query, (id,))
             conn.commit()
-            print(f"Patient with ID: {patient_id} was deleted.")
+            print(f"Patient with ID: {id} was deleted.")
         else:
             print(f"Patient ID is not found.")
 
-    def update_info(self, cursor, conn, patient_id):
-        query = "UPDATE patients SET name = ?, age = ?, gender = ?, phone = ?, email = ?, disease = ? WHERE patient_id = ?"
-        values = (self.name, self.age, self.gender, self.phone, self.email, self.disease, patient_id)
+    def update_info(self, cursor, conn, id):
+        query = "UPDATE patients SET name = ?, age = ?, gender = ?, phone = ?, email = ? WHERE id = ?"
+        values = (self.name, self.age, self.gender, self.phone, self.email, id)
         cursor.execute(query, values)
         conn.commit()
