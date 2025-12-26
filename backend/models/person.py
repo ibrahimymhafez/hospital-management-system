4e0805acf910c183b000d0492ecefb29f2be9624
Ibrahim Hafez <71508496+ibrahimymhafez@users.noreply.github.com>
2025-12-26 13:49:26 +0200

    Expand Person class constructor and Add methods
    Expand Person class constructor to include more fields and add some generic methods such as save, delete, and update

--- backend/models/person.py
+++ backend/models/person.py
@@ -1,4 +1,16 @@
 class Person:
-    def __init__(self, name):
+    def __init__(self, name, age, gender, phone, email):
         self.name = name
+        self.age = age
+        self.gender = gender
+        self.phone = phone
+        self.email = email
         
+    def save_to_db(self, cursor, conn):
+        pass 
+
+    def delete_from_db(cursor, conn, person_id):
+        pass
+
+    def update_info(self, cursor, conn, person_id):
+        pass
