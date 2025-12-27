41ab6d59c1520bf53bfb00129c2aa27416f26048
Ahmed77669 <ahmedyousefk6@gmail.com>
2025-12-28 01:34:42 +0200

    added Authentication screens login and signup , added validation to them , add dummy_dashboard to test program , fixed main.py

---
+++ .history/main_20251228012932.py
@@ -0,0 +1,68 @@
+import backend.database.connectDB as connectDB
+from backend.models.doctor import Doctor
+from backend.models.user import User
+from backend.controllers.auth import Auth
+
+
+import customtkinter as ctk
+from ui.login_screen import LoginScreen
+from ui.dashboard_screen import DashboardScreen
+
+def main():
+    # doctor = Doctor("lol", "surgery", department_id=1)
+    
+    # doctor.save_to_db(cursor, connect)
+    # doctor.delete_from_db(cursor, connect, 73)
+    
+
+    # user = User("Ahmed", "12345678")
+    # auth.signUp("Ahmed", "12345678")
+    user = User("Ahmed", "12345678")
+    auth = Auth()
+    # auth.signUp("Ahmed", "12345678")
+    auth.signIn("Ahmed", "123456788")
+    # user.create_table(cursor, connect)
+    # user.update_user_info(cursor, connect, 1, "AhmedYoussef", "123456789898")
+    
+class HospitalApp(ctk.CTk):  
+    def __init__(self):
+        super().__init__()
+
+        self.title("Hospital Management System") 
+        self.geometry("1100x700")
+
+        self.container = ctk.CTkFrame(self)         
+        self.container.pack(side="top", fill="both", expand=True)
+        self.container.grid_rowconfigure(0, weight=1)
+        self.container.grid_columnconfigure(0, weight=1)
+        self.__current_user = None
+
+        self.frames = {}
+        
+        for F in (LoginView, DashboardScreen, SignUpView):
+            page_name = F.__name__
+            frame = F(parent=self.container, controller=self)
+            self.frames[page_name] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame("LoginView")
+
+    def show_frame(self, page_name):
+        frame = self.frames[page_name]
+        frame.tkraise()
+
+    def set_current_user(self, user):
+        self.__current_user = user
+    
+    def get_current_user(self):
+        return self.__current_user
+    
+def main():
+    hospital_app = HospitalApp()
+    hospital_app.mainloop()
+
+if __name__ == "__main__":
+    main()
+  
+    HospitalApp = HospitalApp()
+    HospitalApp.mainloop()
---
+++ .history/main_20251228013005.py
@@ -0,0 +1,51 @@
+import backend.database.connectDB as connectDB
+from backend.models.doctor import Doctor
+from backend.models.user import User
+from backend.controllers.auth import Auth
+
+
+import customtkinter as ctk
+from UI.login_view import LoginView
+from UI.dummy_dashboard import DashboardScreen
+from UI.signup_view import SignUpView
+    
+class HospitalApp(ctk.CTk):  
+    def __init__(self):
+        super().__init__()
+
+        self.title("Hospital Management System") 
+        self.geometry("1100x700")
+
+        self.container = ctk.CTkFrame(self)         
+        self.container.pack(side="top", fill="both", expand=True)
+        self.container.grid_rowconfigure(0, weight=1)
+        self.container.grid_columnconfigure(0, weight=1)
+        self.__current_user = None
+
+        self.frames = {}
+        
+        for F in (LoginView, DashboardScreen, SignUpView):
+            page_name = F.__name__
+            frame = F(parent=self.container, controller=self)
+            self.frames[page_name] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame("LoginView")
+
+    def show_frame(self, page_name):
+        frame = self.frames[page_name]
+        frame.tkraise()
+
+    def set_current_user(self, user):
+        self.__current_user = user
+    
+    def get_current_user(self):
+        return self.__current_user
+    
+def main():
+    hospital_app = HospitalApp()
+    hospital_app.mainloop()
+
+if __name__ == "__main__":
+    main()
+  
